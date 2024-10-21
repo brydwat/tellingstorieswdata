91095e5b21bf440ca21a861cb3b9f4031406d56d
brydwat <watson.bryce.d@gmail.com>
2024-10-20 19:34:23 -0700

    First commit after initial commit. Original repository had to be deleted due to two independent and non mergeable branches (not easily anyway). Restarted to learn how to set up a git repository and sync it more clearly. Moving forward in this textbook from here.

--- telling_stories_Ch3_Reproducible_Workflows.R
+++ telling_stories_Ch3_Reproducible_Workflows.R
@@ -15,3 +15,23 @@ library(tidyverse)
 library(tinytex)
 library(usethis)
 
+# Focused: Often having an aim to “learn R” or similar tends to be problematic,
+# because there is no real end point to that. It tends to be more efficient to
+# have smaller, more specific goals, such as “make a histogram about the 2022
+# Australian Election with ggplot2”. This is something that can be focused on 
+# and achieved in a few hours. The issue with goals that are more nebulous, 
+# such as “I want to learn R”, is that it is easier to get lost on tangents 
+# and more difficult to get help. This can be demoralizing and lead to people 
+# quitting too early.
+
+# Curious
+
+# Pragmatic
+
+# Tenacious
+
+# Planned
+
+# Done is better than perfect
+
+# Mentality tips from ch 3.5.3
